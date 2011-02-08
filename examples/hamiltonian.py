22e63006b42af785ff7dd48812df173654b35502
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2011-02-08 17:42:57 +0100

    Added 3 examples from pyglpk

---
+++ examples/hamiltonian.py
@@ -0,0 +1,101 @@
+"""
+Taken from pyglpk examples
+"""
+import yaposib
+
+def hamiltonian(edges):
+    node2colnums = {} # Maps node to col indices of incident edges.
+    for colnum, edge in enumerate(edges):
+        n1, n2 = edge
+        node2colnums.setdefault(n1, []).append(colnum)
+        node2colnums.setdefault(n2, []).append(colnum)
+
+    print node2colnums
+
+    lp = yaposib.Problem("Clp")              # A new empty linear program
+    for i in range(len(edges)):
+        col = lp.cols.add(yaposib.vec([]))  # A struct var for each edge
+        col.integer = True                  # Make integer, not continuous
+        col.lowerbound = 0                  # Make binary, not continuous
+        col.upperbound = 1                  # Make binary, not continuous
+
+    # For each node, select at least 1 and at most 2 incident edges.
+    for edge_column_nums in node2colnums.values():
+        row = lp.rows.add(yaposib.vec([(cn, 1.0) for cn in edge_column_nums]))
+        row.lowerbound = 1
+        row.upperbound = 2
+
+    # We should select exactly (number of nodes - 1) edges total
+    row = lp.rows.add(yaposib.vec([(cn, 1.0) for cn in
+        range(len(lp.cols))]))
+    row.lowerbound = row.upperbound = len(node2colnums)-1
+
+    lp.solve()
+    if lp.status != 'optimal': return None  # If no relaxed sol., no exact sol.
+
+    # Return the edges whose associated struct var has value 1.
+    return [edge for edge, col in zip(edges, lp.cols) if col.solution > 0.99]
+
+
+#  1----2----3----5
+#        \  /
+#         \/  Has one H path!
+#         4
+
+g1 = [(1,2), (2,3), (3,4), (4,2), (3,5)]
+print hamiltonian(g1)
+
+#  4    5    6
+#  |    |    |
+#  |    |    | Has no H path!
+#  1----2----3
+
+g2 = [(1,2), (2,3), (1,4), (2,5), (3,6)]
+print hamiltonian(g2)
+
+#  4    5----6
+#  |    |    |
+#  |    |    | Has two H paths!
+#  1----2----3
+
+g3 = g2 + [(5,6)]
+print hamiltonian(g3)
+
+
+def tsp(edges):
+    node2colnums = {} # Maps node to col indices of incident edges.
+    for colnum, edge in enumerate(edges):
+        n1, n2, cost = edge
+        node2colnums.setdefault(n1, []).append(colnum)
+        node2colnums.setdefault(n2, []).append(colnum)
+
+    lp = yaposib.Problem("Clp")              # A new empty linear program
+    for i in range(len(edges)):
+        col = lp.cols.add(yaposib.vec([]))  # A struct var for each edge
+        col.integer = True                  # Make binary, not continuous
+        col.lowerbound = 0              # Either edge selected (1) or not (0)
+        col.upperbound = 1              # Either edge selected (1) or not (0)
+
+    lp.rows.add(len(node2colnums)+1)    # Constraint for each node
+
+    lp.obj[:] = [e[-1] for e in edges]  # Try to minimize the total costs.
+    lp.obj.maximize = False
+
+    # For each node, select two edges, i.e.., an arrival and a departure.
+    for edge_column_nums in node2colnums.values():
+        row = lp.rows.add(yaposib.vec([(cn, 1.0) for cn in edge_column_nums]))
+        row.lowerbound = row.upperbound = 2
+
+    # We should select exactly (number of nodes) edges total
+    row = lp.rows.add(yaposib.vec([(cn, 1.0) for cn in
+        range(len(lp.cols))]))
+    row.lowerbound = row.upperbound = len(node2colnums)
+
+    lp.solve()
+    if lp.status != 'optimal': return None  # If no relaxed sol., no exact sol.
+
+    lp.solveMIP()
+    if lp.status != 'optimal': return None  # Count not find integer solution!
+
+    # Return the edges whose associated struct var has value 1.
+    return [edge for edge, col in zip(edges, lp.cols) if col.value > 0.99]
---
+++ examples/maxflow.py
@@ -0,0 +1,50 @@
+"""
+Taken from pyglpk examples
+"""
+import yaposib
+
+def maxflow(capgraph, s, t):
+    node2rnum = {}                      # Map non-source/sink nodes to row num.
+    for nfrom, nto, cap in capgraph:
+        if nfrom!=s and nfrom!=t:
+            node2rnum.setdefault(nfrom, len(node2rnum))
+        if nto!=s and nto!=t:
+            node2rnum.setdefault(nto, len(node2rnum))
+
+    lp = yaposib.Problem("Clp")         # Empty LP instance.
+    for i in range(len(capgraph)):
+        lp.cols.add(yaposib.vec([])) # As many columns cap-graph edges.
+
+    mat = []                            # Will hold constraint matrix entries.
+    for colnum, (nfrom, nto, cap) in enumerate(capgraph):
+        lp.cols[colnum].lowerbound = 0  # Flow along edge bounded by capacity.
+        lp.cols[colnum].upperbound = cap # Flow along edge bounded by capacity.
+
+        if nfrom == s:
+            lp.obj[colnum] =  1.0       # Flow from source increases flow value
+        elif nto == s:
+            lp.obj[colnum] = -1.0       # Flow to source decreases flow value
+
+        if nfrom in node2rnum:          # Flow from node decreases its net flow
+            mat.append((node2rnum[nfrom], colnum, -1.0))
+        if nto in node2rnum:            # Flow to node increases its net flow
+            mat.append((node2rnum[nto], colnum, 1.0))
+
+    lp.obj.maximize = True              # Want source s max flow maximized.
+
+    for row_nr in range(len(node2rnum)):
+        to_add = [(j, coef) for i, j, coef in mat if i == row_nr]
+        row = lp.rows.add(yaposib.vec(to_add))
+        row.lowerbound = row.upperbound = 0 # Net flow for non-source/sink is 0.
+
+    lp.solve()                        # This should work unless capgraph bad.
+
+    return [(nfrom, nto, col.solution)     # Return edges with assigned flow.
+            for col, (nfrom, nto, cap) in zip(lp.cols, capgraph)]
+
+capgraph = [ ('s','o',3), ('s','p',3), ('o','p',2), ('o','q',3),
+             ('p','r',2), ('q','r',4), ('q','t',2), ('r','t',3) ]
+print maxflow(capgraph, 's', 't')
+
+capgraph = [('s','a',4), ('s','b',1), ('a','b',2.5), ('a','t',1), ('b','t',4)]
+print maxflow(capgraph, 's', 't')
