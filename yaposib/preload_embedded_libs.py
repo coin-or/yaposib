e7b6c2a83e5913219e07043089bcd998c20435e4
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2011-02-10 19:14:41 +0100

    Added the fortran lib in the egg

--- yaposib/preload_embedded_libs.py
+++ yaposib/preload_embedded_libs.py
@@ -10,6 +10,8 @@ if platform.system() == "Linux":
     if platform.uname()[4] == "x86_64":
         lib_dir = os.path.join("embedded_libs", "x86_64")
         to_preload = [
+        "libboost_python-py26.so.1.42.0",
+        "libgfortran.so.3.0.0",
         "libblas.so.3gf",
         "liblapack.so.3gf.0",
         "libCoinUtils.so.0.0.0",
