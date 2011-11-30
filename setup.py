35c8ff4b3740b1b077ae6191c0fd948d1afbe580
Christophe-Marie Duquesne <chm.duquesne@gmail.com>
2011-11-30 19:28:52 +0100

    updating setup to make a new release (with an updated readme)

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ def yaposib_extension():
             )
 
 setup(name="yaposib",
-      version="0.2.74",
+      version="0.2.75",
       description= __doc__,
       long_description = open("README").read(),
       license = open("COPYING").read(),
